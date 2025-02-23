6458182ab17353fa1df17d769e76d79744454032
Artem Chistyakov <archiwebdev@gmail.com>
2025-02-23 16:51:55 +0500

    Delete book by id

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,9 +1,15 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { deleteBook } from '../../redux/books/actionCreators';
 import './BookList.css'
 
 const BookList = () => {
   const books = useSelector((state) => state.books)
-  console.log(books)
+  const dispatch = useDispatch();
+
+  const handleDeleteBook = (id) => {
+    dispatch(deleteBook(id))
+  }
+
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -15,6 +21,9 @@ const BookList = () => {
             <li key={book.id}>
               <div className="book-info">
                 {++index}. {book.title} by <strong>{book.author}</strong>
+              </div>
+              <div className="book-actions">
+                <button onClick={() => handleDeleteBook(book.id)}>Delete</button>
               </div>
             </li>
           ))}
