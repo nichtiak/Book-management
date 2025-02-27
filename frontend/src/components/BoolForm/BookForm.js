8b8fad6d6e4d1e634b9fa713cb688f1677685635
Artem Chistyakov <archiwebdev@gmail.com>
2025-02-28 00:15:23 +0500

    Moved isLoadingViaAPI to redux

--- frontend/src/components/BoolForm/BookForm.js
+++ frontend/src/components/BoolForm/BookForm.js
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { FaSpinner } from 'react-icons/fa'
 // import axios from 'axios'
 // import { addBook } from '../../redux/books/actionCreators'
-import { addBook, fetchBook } from '../../redux/slices/booksSlice'
+import { addBook, fetchBook, selectIsLoadingViaAPI } from '../../redux/slices/booksSlice'
 import { setError } from '../../redux/slices/errorSlice'
 import booksData from '../../data/books.json'
 import createBookWithID from '../../utils/createBookWithID'
@@ -12,7 +12,8 @@ import './BookForm.css'
 const BookForm = () => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
+  // const [isLoading, setIsLoading] = useState(false)
+  const IsLoadingViaAPI = useSelector(selectIsLoadingViaAPI) // подписка на изменение isLoading через useSelector в Redux
   const dispatch = useDispatch()
 
   const handleAddRandomBook = () => {
@@ -46,13 +47,8 @@ const BookForm = () => {
   //   }
   // }
 
-  const handleAddRandomBookViaAPI = async () => {
-    try {
-      setIsLoading(true)
-      await dispatch(fetchBook('http://localhost:4000/random-book-delayed'))   
-    } finally {
-      setIsLoading(false)
-    }
+  const handleAddRandomBookViaAPI = () => {
+    dispatch(fetchBook('http://localhost:4000/random-book-delayed')) 
   }
 
   return (
@@ -81,8 +77,8 @@ const BookForm = () => {
         <button type="button" onClick={handleAddRandomBook}>
           Add Random
         </button>
-        <button type="button" onClick = {handleAddRandomBookViaAPI} disabled={isLoading}>
-          { isLoading ? (
+        <button type="button" onClick = {handleAddRandomBookViaAPI} disabled={IsLoadingViaAPI}>
+          { IsLoadingViaAPI ? (
             <>
               <span>Loading Book...</span>
               <FaSpinner className="spinner" />
--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -3,7 +3,10 @@ import axios from 'axios'
 import createBookWithID from '../../utils/createBookWithID'
 import { setError } from './errorSlice'
 
-const initialState = []
+const initialState = {
+  books: [],
+  isLoadingViaAPI: false
+}
 
 export const fetchBook = createAsyncThunk(
   'books/fetchBook',
@@ -13,7 +16,8 @@ export const fetchBook = createAsyncThunk(
       return res.data
     } catch (error) {
       thunkAPI.dispatch(setError(error.message))
-      throw error
+      return thunkAPI.rejectWithValue(error) // option 1
+      // throw error       OPTION 2
     }
   }
 )
@@ -23,27 +27,46 @@ const booksSlice = createSlice({
   initialState,
   reducers: {
     addBook: (state, action) => {
-        state.push(action.payload)
+        state.books.push(action.payload)
       },
     deleteBook: (state, action) => {
-      return state.filter((book) => book.id !== action.payload);
+      return {...state, books: state.books.filter((book) => book.id !== action.payload)};
     },
     toggleFavorite: (state, action) => {
-      return state.map((book) => (book.id === action.payload ? { ...book, isFavorite: !book.isFavorite } : book));
+      // return state.books.map((book) => (book.id === action.payload ? book.isFavorite = !book.isFavorite : book));
+      state.books.forEach((book) => {
+        if (book.id === action.payload) {
+          book.isFavorite = !book.isFavorite
+        }
+      })
     }
   },
   // option 1
   // extraReducers: {
+  //   [fetchBook.pending]: (state) => {
+  //     state.isLoadingViaAPI = true
+  //   },
   //   [fetchBook.fulfilled]: (state, action) => {
+  //     state.isLoadingViaAPI = false
   //     if (action.payload.title && action.payload.author) {
-  //       state.push(createBookWithID(action.payload, 'API'))
+  //       state.books.push(createBookWithID(action.payload, 'API'))
   //     }
   //   },
-  // }
+  //   [fetchBook.rejected]: (state) => {
+  //     state.isLoadingViaAPI = false
+  //   },
+  // },
     // option 2
     extraReducers: (builder) => {
+      builder.addCase(fetchBook.pending, (state) => {
+        state.isLoadingViaAPI = true
+      })
       builder.addCase(fetchBook.fulfilled, (state, action) => {
-        state.push(createBookWithID(action.payload, 'API'))
+        state.isLoadingViaAPI = false
+        state.books.push(createBookWithID(action.payload, 'API'))
+      })
+      builder.addCase(fetchBook.rejected, (state) => {
+        state.isLoadingViaAPI = false
       })
     }
 })
@@ -62,7 +85,8 @@ export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions
 //   }
 // }
 
-export const selectBooks = (state) => state.books
+export const selectBooks = (state) => state.books.books
+export const selectIsLoadingViaAPI = (state) => state.books.isLoadingViaAPI
 export default booksSlice.reducer
 
 
